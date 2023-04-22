d90dae0a3ef2c7c76f5a30b182e6f9e9ae9f4235
Minisiia <ar_tik@yahoo.com>
2023-04-22 20:20:42 +0300

    Java Advanced 2. Collections

--- src/list/Handler.java
+++ src/list/Handler.java
@@ -1,19 +1,17 @@
 package list;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Handler {
 
     public static List<String> fillList() {
         Scanner scanner = new Scanner(System.in);
         List<String> list = new ArrayList<>();
-        String line = null;
         System.out.println("Enter a lines using enter or end to exit:");
-        while (true) {
-            line = scanner.nextLine();
-            if (!line.equals("end")) list.add(line);
-            else break;
-        }
+        Stream.generate(scanner::nextLine)
+                .takeWhile(line -> !line.equals("end"))
+                .forEach(list::add);
         return list;
     }
 
